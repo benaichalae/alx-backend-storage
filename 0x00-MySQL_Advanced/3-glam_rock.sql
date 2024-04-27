9e8753f8b984bf7c1720cbf9e928fe80eb5ab891
benaichalae <benaichalae@gmail.com>
2024-04-27 09:22:25 -0700

    sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- SQL script that lists all bands with Glam rock as their main style
-SELECT band_name,
-       (2022 - SUBSTRING_INDEX(formed, '-', -1)) AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT band_name,(2022 - SUBSTRING_INDEX(formed, '-', -1)) AS lifespan
+    FROM metal_bands
+    WHERE main_style = 'Glam rock'
+    ORDER BY lifespan DESC;
